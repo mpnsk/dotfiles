3a864298b44ffad854953a8a033e5e81378d5cf2
mpnsk <sINFmapaun@fh-rosenheim.de>
2015-07-25 01:57:38 +0200

    added install scripts

---
+++ install/gnome-terminal-solarized.sh
@@ -0,0 +1,3 @@
+git clone https://github.com/Anthony25/gnome-terminal-colors-solarized.git ../gnome-terminal-colors-solarized
+echo 'running install.sh ..'
+../gnome-terminal-colors-solarized/install.sh
---
+++ install/link-dotfiles.sh
@@ -0,0 +1,10 @@
+cd
+ln -s .dotfiles/.gitconfig
+ln -s .dotfiles/.inputrc
+ln -s .dotfiles/.my.cnf
+ln -s .dotfiles/.vimrc
+ln -s .dotfiles/.XCompose
+echo 'source ~/.dotfiles/.alias' >> .bashrc
+echo 'source ~/.dotfiles/.bashrc' >> .bashrc
+echo 'source ~/.dotfiles/.env' >> .bashrc
+echo 'source ~/.dotfiles/.func' >> .bashrc
