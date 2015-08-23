33dccfe9dd7e60db30233b2901ba06637971b481
mpnsk <sINFmapaun@fh-rosenheim.de>
2015-08-23 17:48:32 +0200

    fixed a typo

--- install/dircolors-solarized.sh
+++ install/dircolors-solarized.sh
@@ -1,4 +1,4 @@
 git clone https://github.com/seebi/dircolors-solarized.git ../dircolors-solarized
-echo '#dircolors-solarized' >> ../env
-echo 'eval `dircolors ~/.dotfiles/dircolors-solarized/dircolors.256dark`' >> ../env
-echo 'export TERM=xterm-256color ' >> ../env
+echo '#dircolors-solarized' >> ../.env
+echo 'eval `dircolors ~/.dotfiles/dircolors-solarized/dircolors.256dark`' >> ../.env
+echo 'export TERM=xterm-256color ' >> ../.env
